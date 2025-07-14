bba1f3dab5dca70e52b64fd1a28fbc4ded95cfbd
koutekulta <koutekulta@gmail.com>
2025-07-15 00:51:02 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Bokep Tante Diwarung';
-export const desk = 'Video Bokep Indo Jepang Jav Hd Tante Viral Terbaru Bocil Ngentot Jilbab Smp Skandal';
-export const url = 'https://bokeptantediwarung.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
